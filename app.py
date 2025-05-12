bd51c9c770db311e174550ac07ade2410f8f72a2
tuanduygeo <87938671+tuanduygeo@users.noreply.github.com>
2025-05-12 07:34:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -369,9 +369,9 @@ st.image("https://sosrff.tsu.ru/new/shm.jpg", caption="Schumann Resonance - Live
 # --- ĐỊA TỪ TRẠM PHÚ THỌ (INTERMAGNET) ---
 st.markdown("""
 ### 4.🧲 Dữ liệu địa từ trực tuyến""")
-start_date = (datetime.today() - timedelta(days=15)).strftime('%Y-%m-%d')
+start_date = (datetime.today() - timedelta(days=30)).strftime('%Y-%m-%d')
 end_date = datetime.today().strftime('%Y-%m-%d')
-https://imag-data.bgs.ac.uk/GIN_V1/GINForms2?observatoryIagaCode=PHU&publicationState=Best+available&dataStartDate={start_date}&dataDuration=15&samplesPerDay=minute&submitValue=View+%2F+Download&request=DataView
+https://imag-data.bgs.ac.uk/GIN_V1/GINForms2?observatoryIagaCode=PHU&publicationState=Best+available&dataStartDate={start_date}&dataDuration=30&samplesPerDay=minute&submitValue=View+%2F+Download&request=DataView
 st.components.v1.iframe(iframe_url, height=1000,scrolling=True)
 
 st.caption("📍 Phát triển từ tác giả Nguyễn Duy Tuấn – với mục đích phụng sự tâm linh và cộng đồng.")
