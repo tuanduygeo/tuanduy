b0358cbc368e584fe868c6c166777429f8ce22f7
tuanduygeo <87938671+tuanduygeo@users.noreply.github.com>
2025-05-14 16:03:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -639,4 +639,6 @@ try:
 except Exception as e:
     st.error("❌ Lỗi khi tải dữ liệu Kp Index.")
     st.text(str(e))
-st.header("📍 Tác giả Nguyễn Duy Tuấn – với mục đích phụng sự tâm linh và cộng đồng.SĐT&ZALO: 0377442597.DONATE: nguyenduytuan techcombank 19033167089018")
+st.markdown("""
+### Tác giả Nguyễn Duy Tuấn – với mục đích phụng sự tâm linh và cộng đồng.SĐT&ZALO: 0377442597.DONATE: nguyenduytuan techcombank 19033167089018
+""")
