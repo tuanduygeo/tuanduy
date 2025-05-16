052542d930b159e50622b03c5ae3cba0432be0f8
tuanduygeo <87938671+tuanduygeo@users.noreply.github.com>
2025-05-16 13:54:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,25 +23,7 @@ if "selected_idx" not in st.session_state:
 html_dir = "dulieu"
 html_files = sorted([f for f in os.listdir(html_dir) if f.endswith(".html")])
 df = pd.DataFrame({"Tên công trình": html_files})
-# Phân trang
-per_page = 8
-total_pages = math.ceil(len(df) / per_page)
-page = st.number_input(f"📄 Trang (1–{total_pages}):", min_value=1, max_value=total_pages, value=1, step=1)
-
-start_idx = (page - 1) * per_page
-end_idx = start_idx + per_page
-df_page = df.iloc[start_idx:end_idx]
-# Hiển thị danh sách từng trang
-for i, (_, row) in enumerate(df_page.iterrows()):
-    idx = start_idx + i
-    col1, col2 = st.columns([5, 1])
-    with col1:
-        st.markdown(f"🔸 **{row['Tên công trình']}**")
-    with col2:
-        if st.button("Xem", key=row['Tên công trình']):
-            st.session_state.selected_idx = idx
-
-
+st.dataframe(df, height=180)
 
 
 # Hiển thị bản đồ
