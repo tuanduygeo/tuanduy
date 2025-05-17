53b76fd9585cc717af098a9c79cfcf1bbc4c3216
tuanduygeo <87938671+tuanduygeo@users.noreply.github.com>
2025-05-17 11:39:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -958,7 +958,9 @@ if st.button("run"):
     # Vẽ contour
     levels = np.linspace(data_array.min(), data_array.max(), 21)
     cf = ax.contourf(Xx3857, Yx3857, data_array, cmap="rainbow", levels=levels, alpha=0.5)
-    
+    threshold = np.percentile(data_array, 75)
+    mask = data_array >= threshold
+    ax.contour(Xx3857, Yx3857, mask, levels=[0.5], colors='red', linewidths=2)
     # Vẽ vòng Fibonacci
     plot_fibonacci_labels_only(ax, x_center, y_center, labels_24, radius=500)
     # Slider góc
