f71f51464f6326f5ef1722039004ed32a20b4537
tuanduygeo <87938671+tuanduygeo@users.noreply.github.com>
2025-05-19 17:22:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -281,10 +281,10 @@ def main():
                 arrow_length = radius * 0.75
                 if manual_bearing is not None:
                     # Vẽ theo góc nhập tay (màu đỏ)
-                    angle_rad = np.deg2rad(manual_bearing)
-                    arrow_dx = np.cos(angle_rad) * arrow_length
-                    arrow_dy = np.sin(angle_rad) * arrow_length
-                    arrow_color = 'red'
+                    angle_rad = np.deg2rad(manual_bearing)-np.pi/2
+                    arrow_dx = np.sin(angle_rad) * arrow_length
+                    arrow_dy = np.cos(angle_rad) * arrow_length
+                    arrow_color = 'white'
                 else:
                     # Vẽ theo hướng DEM tự động (màu trắng)
                     arrow_dx = -dir_x * arrow_length
