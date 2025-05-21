150cb4b24b7342920636a8f8d7dc853de728c798
tuanduygeo <87938671+tuanduygeo@users.noreply.github.com>
2025-05-21 15:35:11 +0700

    Update astrology_utils.py

--- astrology_utils.py
+++ astrology_utils.py
@@ -1128,7 +1128,7 @@ def astrology_block():
     chart_df["Năm_mới"] = chart_df["Năm"] - birth_x
     
     
-    fig, ax = plt.subplots(figsize=(12, 4))
+    fig, ax = plt.subplots(figsize=(12, 6))
     ax.plot(chart_df["Năm_mới"], chart_df["Điểm số"], marker='o')
     ax.hlines(y=0, xmin=0, xmax=115, color='black', linestyle='-', linewidth=2)
 
