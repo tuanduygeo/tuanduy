a9d0c0c0529b04cd69a0a5266d1556c579d4d462
tuanduygeo <87938671+tuanduygeo@users.noreply.github.com>
2025-05-24 12:48:27 +0700

    Update astrology_utils.py

--- astrology_utils.py
+++ astrology_utils.py
@@ -1427,7 +1427,7 @@ def astrology_block():
     
 
     # Sử dụng dữ liệu df_dasha, planet_data và jd ngày sinh
-    chart_df, birth_x = build_life_chart(df_dasha, planet_data,birth_jd,, df_planets, planet_to_ruled_houses, scoring_config)
+    chart_df, birth_x = build_life_chart(df_dasha, planet_data,birth_jd, df_planets, planet_to_ruled_houses, scoring_config)
     
     # Vẽ biểu đồ zigzag và đường cong mượt
     chart_df["Năm_mới"] = chart_df["Năm"] - birth_x
