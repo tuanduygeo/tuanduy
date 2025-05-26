02d5c761fed7dc8fdd0d1ebd3dab03f76d187b3d
tuanduygeo <87938671+tuanduygeo@users.noreply.github.com>
2025-05-26 15:09:19 +0700

    Create appchiemtinh.py

---
+++ appchiemtinh.py
@@ -0,0 +1,8 @@
+import streamlit as st
+from astrology_utils import astrology_block
+
+def main():
+    astrology_block()  # Toàn bộ giao diện và logic đã nằm trong hàm này
+
+if __name__ == "__main__":
+    main()
