be39fbe34813fca6a893862c90ecb517ac54a359
tuanduygeo <87938671+tuanduygeo@users.noreply.github.com>
2025-05-27 10:47:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -310,12 +310,12 @@ def main():
         distance_between_zones = st.number_input("Mạch chính rộng (m)", min_value=1.0, max_value=800.0, value=30.0, step=1.0)
         offset_d = st.number_input("Dịch chuyển ngang-chính (m)", min_value=-500.0, max_value=500.0, value=0.0, step=1.0)
         rotate_angle = st.number_input("Góc lệch mạch-chính (độ)", min_value=-180.0, max_value=180.0, value=0.0, step=1.0)
-        ratio_red=st.number_input("tỷ số tốt/xấu- chính", min_value=0.0, max_value=1.0, value="", step=0.2, format="%.1f")
+        ratio_red=st.number_input("tỷ số tốt/xấu- chính", min_value=0.0, max_value=1.0, value="0.5", step=0.2, format="%.1f")
     with col3:
         distance_between_zones2 = st.number_input("Mạch phụ rộng(m)", min_value=1.0, max_value=800.0, value=30.0, step=1.0)
         offset_d2 = st.number_input("Dịch chuyển ngang-phụ (m)", min_value=-500.0, max_value=500.0, value=0.0, step=1.0)
         rotate_angle2 = st.number_input("Góc lệch mạch-phụ (độ)", min_value=-180.0, max_value=180.0, value=0.0, step=1.0)
-        ratio_red2=st.number_input("tỷ số tốt/xấu- phụ", min_value=0.0, max_value=1.0, value="", step=0.2, format="%.1f")
+        ratio_red2=st.number_input("tỷ số tốt/xấu- phụ", min_value=0.0, max_value=1.0, value="0.5", step=0.2, format="%.1f")
     with col4:
         manual_bearing = st.number_input("góc", min_value=0.0, max_value=360.0, value=None, step=1.0, format="%.1f")
         dt = st.number_input("Bán kính", min_value=0.001, max_value=0.02, value=0.005, step=0.002, format="%.3f")
@@ -745,31 +745,12 @@ def main():
                 value_center = data_array[i_center, j_center]
                 # Xác định median
                 median_z = np.median(data_array)
-                ratio_red = 0.5
-                ratio_red2 = 0.5
-                ratio_redauto = ratio_red
-                ratio_redauto2 = ratio_red2
-                # Xác định offset_d để màu đúng ý
                 if value_center > median_z:
                     st.write(f'Giá trị tại tâm ({value_center:.2f}) > median ({median_z:.2f}): **CAO**')
-                    ratio_redauto = ratio_redauto + 0.1
-                    ratio_redauto2 = ratio_redauto2 + 0.1
+                    
                 else:
                     st.write(f'Giá trị tại tâm ({value_center:.2f}) < median ({median_z:.2f}): **THẤP**')
-                    ratio_redauto = ratio_redauto - 0.1
-                    ratio_red2auto = ratio_red2auto - 0.1
-                if ratio_red2_input.strip() != "":
-                    try:
-                        ratio_red2 = float(ratio_red2_input)
-                        ratio_red2 = min(max(ratio_red2, 0), 1)  # Giới hạn từ 0-1
-                        st.write(f"Tỷ số tốt/xấu (theo nhập): {ratio_red2:.2f}")
-                    except:
-                        st.warning("Vui lòng nhập số hợp lệ trong khoảng 0–1")
-                        ratio_red2 = ratio_red2_auto
-                        st.write(f"Tỷ số tốt/xấu (tự động): {ratio_red2:.2f}")
-                else:
-                    ratio_red2 = ratio_red2_auto
-                    st.write(f"Tỷ số tốt/xấu (tự động): {ratio_red2:.2f}")
+                    
                 dt2 = dt / 10
                 fig2, ax2 = plt.subplots(figsize=(12, 12))
                 x0, x1 = x_center - radius/5, x_center + radius/5
