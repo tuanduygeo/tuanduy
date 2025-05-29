1d60a6857bfc7b75735351943db94c92c5e7bc20
tuanduygeo <87938671+tuanduygeo@users.noreply.github.com>
2025-05-29 12:24:34 +0700

    Update astrology_utils.py

--- astrology_utils.py
+++ astrology_utils.py
@@ -51,7 +51,7 @@ def plot_antardasha_multi_column(df_antar, ncol=2, fontsize=15, cell_height=0.65
     )
     table.auto_set_font_size(False)
     table.set_fontsize(fontsize)
-    table.scale(1.14, 1.18)
+    table.scale(1.14, 1.35)
 
     # Tô màu header cho dễ nhìn
     for (row, col), cell in table.get_celld().items():
