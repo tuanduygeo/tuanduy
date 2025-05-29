76393c76c9737448d98379a48a02b7da58abf4d7
tuanduygeo <87938671+tuanduygeo@users.noreply.github.com>
2025-05-29 12:33:46 +0700

    Update astrology_utils.py

--- astrology_utils.py
+++ astrology_utils.py
@@ -10,7 +10,7 @@ import io
 from io import BytesIO
 import textwrap
 from PIL import Image
-def plot_antardasha_multi_column(df_antar, ncol=2, fontsize=15, cell_height=1):
+def plot_antardasha_multi_column(df_antar, ncol=2, fontsize=15, cell_height=0.75):
     # Bỏ cột Số năm nếu có
     if "Số năm" in df_antar.columns:
         df_antar = df_antar.drop(columns=["Số năm"])
@@ -51,7 +51,7 @@ def plot_antardasha_multi_column(df_antar, ncol=2, fontsize=15, cell_height=1):
     )
     table.auto_set_font_size(False)
     table.set_fontsize(fontsize)
-    table.scale(1.14, 1.35)
+    table.scale(1.5, 1.35)
 
     # Tô màu header cho dễ nhìn
     for (row, col), cell in table.get_celld().items():
